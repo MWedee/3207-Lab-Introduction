68f57d34c65c065f8eb41dd6eada0c6918200549
Mustafa Wedee <104322948+MWedee@users.noreply.github.com>
2023-01-18 17:05:10 -0500

    Update randchar.c

--- randchar.c
+++ randchar.c
@@ -1,3 +1,6 @@
+//Mustafa Wedee
+//01/18/23
+//This .c file is to be used in conjuction with print_char.c, this function returns a random char using rand(); Seeded in main.
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
