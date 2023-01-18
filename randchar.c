cbddd5ce8b4521f17bc44fed06bead8a8eff9a9b
Mustafa Wedee <104322948+MWedee@users.noreply.github.com>
2023-01-18 17:00:57 -0500

    Update randchar.c

--- randchar.c
+++ randchar.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 char randchar(){
-	int r = rand()% (122+1-97)+97;
+	int r = rand()% (122+1-97)+97; //ascii values of lowercase a to z
 	char c = r;
 	return c;
 }
